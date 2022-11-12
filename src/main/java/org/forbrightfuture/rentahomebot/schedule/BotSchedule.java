9532351be492104832f482ca2a9d47fa5a2131bf
Javid Afandiyev <afandiyevcavid@gmail.com>
2022-11-12 16:05:32 +0400

    static variables used for time calculation in jobs

--- src/main/java/org/forbrightfuture/rentahomebot/schedule/BotSchedule.java
+++ src/main/java/org/forbrightfuture/rentahomebot/schedule/BotSchedule.java
@@ -3,11 +3,11 @@ package org.forbrightfuture.rentahomebot.schedule;
 import lombok.extern.slf4j.Slf4j;
 import org.forbrightfuture.rentahomebot.constants.Website;
 import org.forbrightfuture.rentahomebot.dto.HeartBeatUserDTO;
-import org.forbrightfuture.rentahomebot.dto.telegram.send.text.SendMessageDTO;
 import org.forbrightfuture.rentahomebot.dto.telegram.update.TelegramUpdateDTO;
 import org.forbrightfuture.rentahomebot.entity.Home;
 import org.forbrightfuture.rentahomebot.service.*;
 import org.forbrightfuture.rentahomebot.service.broadcast.BroadcastService;
+import org.forbrightfuture.rentahomebot.staticVar.TimeVariables;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -41,31 +41,34 @@ public class BotSchedule {
     @Scheduled(fixedRateString = "${task.update-cities.rate}")
     public void updateCities() throws IOException {
         log.info("City table update began");
-        long startTime = System.currentTimeMillis();
+        TimeVariables.cityUpdateStartTime = System.currentTimeMillis();
         cityService.updateCities();
-        long endTime = System.currentTimeMillis();
+        TimeVariables.cityUpdateEndTime = System.currentTimeMillis();
         log.info("City table update stopped");
-        log.info("Passed time for city table update: " + (endTime - startTime) + " ms");
+        log.info("Passed time for city table update: " +
+                (TimeVariables.cityUpdateEndTime - TimeVariables.cityUpdateStartTime) + " ms");
     }
 
     @Scheduled(fixedDelayString = "${task.binaaz-update-homes.rate}", initialDelay = 10000L)
     public void updateBinaazHomes() throws IOException {
         log.info("Binaaz - homes update began");
-        long startTime = System.currentTimeMillis();
+        TimeVariables.binaazHomesUpdateStartTime = System.currentTimeMillis();
         homeService.findNewHomes(Website.BinaAz);
-        long endTime = System.currentTimeMillis();
+        TimeVariables.binaazHomesUpdateEndTime = System.currentTimeMillis();
         log.info("Binaaz - homes update stopped");
-        log.info("Binaaz - passed time for home table update: " + (endTime - startTime) + " ms");
+        log.info("Binaaz - passed time for home table update: " +
+                (TimeVariables.binaazHomesUpdateEndTime - TimeVariables.binaazHomesUpdateStartTime) + " ms");
     }
 
     @Scheduled(fixedDelayString = "${task.yeniemlak-update-homes.rate}", initialDelay = 20000L)
     public void updateYeniEmlakHomes() throws IOException {
         log.info("Yeniemlak - homes update began");
-        long startTime = System.currentTimeMillis();
+        TimeVariables.yeniemlakHomesUpdateStartTime = System.currentTimeMillis();
         homeService.findNewHomes(Website.YeniEmlak);
-        long endTime = System.currentTimeMillis();
+        TimeVariables.yeniemlakHomesUpdateEndTime = System.currentTimeMillis();
         log.info("Yeniemlak - homes update stopped");
-        log.info("Yeniemlak - passed time for home table update: " + (endTime - startTime) + " ms");
+        log.info("Yeniemlak - passed time for home table update: " +
+                (TimeVariables.yeniemlakHomesUpdateEndTime - TimeVariables.yeniemlakHomesUpdateStartTime) + " ms");
     }
 
     @Scheduled(fixedRateString = "${task.update-telegram-update.rate}")
@@ -86,38 +89,48 @@ public class BotSchedule {
     @Scheduled(fixedRateString = "${task.clear-old-homes.rate}")
     public void clearOldHomesFromDatabase() {
         log.info("Clearing old homes began");
-        long startTime = System.currentTimeMillis();
+        TimeVariables.clearOldHomesStartTime = System.currentTimeMillis();
         homeService.clearOldHomes();
-        long endTime = System.currentTimeMillis();
+        TimeVariables.clearOldHomesEndTime = System.currentTimeMillis();
         log.info("Clearing old homes ended");
-        log.info("Time passed for old homes deletion: " + (endTime - startTime) + " ms");
+        log.info("Time passed for old homes deletion: " +
+                (TimeVariables.clearOldHomesEndTime - TimeVariables.clearOldHomesStartTime) + " ms");
     }
 
     @Scheduled(fixedRateString = "${task.clear-old-messages.rate}")
     public void clearOldMessagesFromDatabase() {
         log.info("Clearing old messages began");
+        TimeVariables.clearOldMessagesStartTime = System.currentTimeMillis();
         chatDataService.deleteOldMessages();
+        TimeVariables.clearOldMessagesEndTime = System.currentTimeMillis();
         log.info("Clearing old messages ended");
+        log.info("Time passed for old messages deletion: " +
+                (TimeVariables.clearOldMessagesEndTime - TimeVariables.clearOldMessagesStartTime) + " ms");
     }
 
     @Scheduled(fixedRateString = "${task.send-heartbeat-signal.rate}")
     public void sendHeartBeatMessages() {
         List<HeartBeatUserDTO> heartBeatUserDTOS = heartBeatUserService.getAllHeartBeatUsers();
         log.info("Heart beat signals will be sent to " + heartBeatUserDTOS.size() + " users");
+        TimeVariables.heartBeatSendingStartTime = System.currentTimeMillis();
         for(HeartBeatUserDTO heartBeatUserDTO: heartBeatUserDTOS) {
             telegramMessagingService.sendHeartBeatMessage(heartBeatUserDTO.getChatId());
         }
+        TimeVariables.heartBeatSendingEndTime = System.currentTimeMillis();
         log.info("Heart beat signals were sent");
+        log.info("Time passed for sending heartbeat message: " +
+                (TimeVariables.heartBeatSendingEndTime - TimeVariables.heartBeatSendingStartTime) + " ms");
     }
 
     @Scheduled(fixedDelayString = "${task.send-broadcast-message.rate}")
     public void sendBroadcastMessages() {
-        long startTime = System.currentTimeMillis();
+        TimeVariables.broadcastSendingStartTime = System.currentTimeMillis();
         if (broadcastService.sendCustomBroadcastMessage()) {
             log.info("Broadcast message was sent!");
         }
-        long endTime = System.currentTimeMillis();
-        log.info("Passed time in broadcast messages: " + (endTime - startTime) + " ms");
+        TimeVariables.broadcastSendingEndTime = System.currentTimeMillis();
+        log.info("Passed time in broadcast messages: " +
+                (TimeVariables.broadcastSendingEndTime - TimeVariables.broadcastSendingStartTime) + " ms");
     }
 
 }
---
+++ src/main/java/org/forbrightfuture/rentahomebot/staticVar/TimeVariables.java
@@ -0,0 +1,28 @@
+package org.forbrightfuture.rentahomebot.staticVar;
+
+public class TimeVariables {
+
+    public static long cityUpdateStartTime;
+    public static long cityUpdateEndTime;
+
+    public static long binaazHomesUpdateStartTime;
+    public static long binaazHomesUpdateEndTime;
+
+    public static long yeniemlakHomesUpdateStartTime;
+    public static long yeniemlakHomesUpdateEndTime;
+
+    public static long clearOldHomesStartTime;
+    public static long clearOldHomesEndTime;
+
+    public static long clearOldMessagesStartTime;
+    public static long clearOldMessagesEndTime;
+
+    public static long heartBeatSendingStartTime;
+    public static long heartBeatSendingEndTime;
+
+    public static long broadcastSendingStartTime;
+    public static long broadcastSendingEndTime;
+
+
+
+}
